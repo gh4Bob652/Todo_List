d0d1dc369c1ba0d47e3ad90f0781657bd1c763db
Bob Skoubo <kickns@gmail.com>
2014-12-15 09:37:06 -0800

    Add specs and code for editing todo items

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe TodoItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it {should belong_to(:todo_list) }
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
